616da44959867dc749f81675091ea1e3a076376c
Sviatoslav Naiden <sviatoslav_naiden@epam.com>
2017-08-14 21:12:03 +0300

    chemdoodle for molecule drawing

--- src/main/java/com/naiden/digitallab/configuration/DefaultView.java
+++ src/main/java/com/naiden/digitallab/configuration/DefaultView.java
@@ -2,6 +2,7 @@ package com.naiden.digitallab.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -16,5 +17,16 @@ public class DefaultView extends WebMvcConfigurerAdapter {
 //        registry.addViewController( "/" ).setViewName( "forward:/home.html" );
 //        regisry.setOrder( Ordered.HIGHEST_PRECEDENCE );
 //        super.addViewControllers( registry );
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        if (!registry.hasMappingForPattern("/css/**")) {
+            registry.addResourceHandler("/css/**").addResourceLocations("classpath:/css/");
+        }
+        if (!registry.hasMappingForPattern("/js/**")) {
+            registry.addResourceHandler("/js/**").addResourceLocations("classpath:/js/");
+        }
     }
 }
