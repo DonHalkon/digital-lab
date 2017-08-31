3b0e1935184ba2b487b989452585013fe27b636c
Sviatoslav Naiden <sviatoslav_naiden@epam.com>
2017-08-31 14:12:25 +0300

    devtools and actuators dependencies are added

--- src/main/java/com/naiden/digitallab/configuration/DefaultView.java
+++ src/main/java/com/naiden/digitallab/configuration/DefaultView.java
@@ -25,9 +25,9 @@ public class DefaultView extends WebMvcConfigurerAdapter {
             registry.addResourceHandler("/bootstrap/**").addResourceLocations("classpath:/bootstrap/");
         }
 
-//        if (!registry.hasMappingForPattern("/templates/fragments/**")) {
-//            registry.addResourceHandler("/templates/fragments/**").addResourceLocations("classpath:/templates.fragments/");
-//        }
+        if (!registry.hasMappingForPattern("/templates/fragments/**")) {
+            registry.addResourceHandler("/templates/fragments/**").addResourceLocations("classpath:/templates.fragments/");
+        }
 
 
         if (!registry.hasMappingForPattern("/js/**")) {
